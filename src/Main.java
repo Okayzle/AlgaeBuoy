90d016007a762700994ed692f61a59dc36454daf
walesaj <walesaj@msoe.edu>
2018-01-31 12:09:53 -0600

    Added Mongo and some stub functions.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDriverInformation;
+import jdk.nashorn.internal.ir.debug.JSONWriter;
 import org.json.simple.JSONObject;
+import sun.net.www.http.HttpClient;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Random;
 
 
@@ -16,7 +27,7 @@ public class Main {
                 obj.put("turbidity", genUnder100Num());
                 obj.put("pH", genPH());
                 // pushes up to the DB
-
+                pushDataToAPI(obj);
                 System.out.println(obj);
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -26,6 +37,24 @@ public class Main {
 
     }
 
+    public static void pushDataToAPI(JSONObject payload) {
+        try {
+            URL url = new URL("localhost:4200/api/upload_data");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection)con;
+            con.setDoInput(true);
+            http.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type","application/json;charset=UTF-8");
+            OutputStream os = http.getOutputStream();
+            os.write(payload.toJSONString().getBytes("UTF-8"));
+
+        }catch (IOException e){
+        }
+    }
+
+    public static void pushDataToMongo(JSONObject payload){
+        MongoClient client = new MongoClient();
+    }
 
     public static int genUnder100Num(){
         return new Random().nextInt(100) +1;
